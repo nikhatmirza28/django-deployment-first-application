e26f1db02e1d5829c8f24dd3732edec28f1b7e13
nikhatmirza28 <nikhatkhanmirza@gmail.com>
2023-12-30 17:33:32 +0530

    firstcommit

---
+++ DemoApp1/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from  django.http import HttpResponse;
+
+# Create your views here.
+def f1(request):
+    return HttpResponse("<h2>Hello From DemoApp1 f1()</h2><hr />");
+def f2(request):
+    return HttpResponse("<h2>Hello from DemoApp1 f2()</h2><hr />");
+
